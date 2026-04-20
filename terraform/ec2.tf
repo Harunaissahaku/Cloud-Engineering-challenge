2eb5ecebc89218367b8cbf344b7ba037a896788f
Harunaissahaku <160567738+Harunaissahaku@users.noreply.github.com>
2026-04-19 20:23:47 -0600

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,1 +1,13 @@
 
+resource "aws_instance" "app" {
+  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 (adjust per region)
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "graylog-app"
+  }
+}
